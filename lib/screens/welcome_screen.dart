4beb081f81c6792f9f0d16316b201b2d620038a8
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-25 14:34:38 -0300

    tela de welcome

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,67 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        decoration: BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+                image: AssetImage("images/bg.png"),
+                fit: BoxFit.cover,
+                opacity: .6)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Coffe Shop",
+                style: GoogleFonts.pacifico(fontSize: 50, color: Colors.white)),
+            Column(
+              children: [
+                Text(
+                  "Sentindo-se triste? tome um gole de café",
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(.8),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                InkWell(
+                  splashColor: Colors.black,
+                  onTap: () {},
+                  child: Ink(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE57734),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Vamos começar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
