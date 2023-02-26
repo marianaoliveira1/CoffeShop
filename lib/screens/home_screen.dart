af21054e3947980818b3c5c9bbced84277937855
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-26 15:18:48 -0300

    tabs e pesquisar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,147 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+
+    super.initState();
+  }
+
+  _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.white.withOpacity(.5),
+                        size: 35,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white.withOpacity(.5),
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "It's a great day for coffe",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 50, 54, 56),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Find your coffe",
+                      hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 30,
+                        color: Colors.white.withOpacity(.5),
+                      )),
+                ),
+              ),
+              TabBar(
+                  controller: _tabController,
+                  labelColor: Color(0xFFE57734),
+                  unselectedLabelColor: Colors.white.withOpacity(.5),
+                  isScrollable: true,
+                  indicator: UnderlineTabIndicator(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Color(0xFFE57734),
+                      ),
+                      insets: EdgeInsets.symmetric(horizontal: 16)),
+                  labelStyle:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  labelPadding: EdgeInsets.symmetric(horizontal: 20),
+                  tabs: [
+                    Tab(
+                      text: "Hot Coffe",
+                    ),
+                    Tab(
+                      text: "Hot Coffe",
+                    ),
+                    Tab(
+                      text: "Hot Coffe",
+                    ),
+                    Tab(
+                      text: "Hot Coffe",
+                    ),
+                  ]),
+              SizedBox(
+                height: 10,
+              ),
+              // Center(
+              //   child: [
+              //     Container(
+
+              //     )
+              //   ],
+              // )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -36,27 +36,28 @@ class WelcomeScreen extends StatelessWidget {
                 SizedBox(
                   height: 80,
                 ),
-                InkWell(
-                  splashColor: Colors.black,
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()));
-                  },
-                  child: Ink(
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-                      decoration: BoxDecoration(
-                        color: Color(0xFFE57734),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        "Vamos começar",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1),
+                Material(
+                  color: Color(0xFFE57734),
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen()));
+                    },
+                    child: Ink(
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                        child: Text(
+                          "Vamos começar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1),
+                        ),
                       ),
                     ),
                   ),
